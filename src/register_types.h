4a7d45c71c2f63de4c3b322fbabf0934387e5724
fenix-hub <n.santilio97@gmail.com>
2022-10-01 14:30:06 +0200

    update h files

--- src/register_types.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <godot_cpp/core/class_db.hpp>
-
-using namespace godot;
-
-void qrnative_initialize(ModuleInitializationLevel p_level);
-void qrnative_terminate(ModuleInitializationLevel p_level);
